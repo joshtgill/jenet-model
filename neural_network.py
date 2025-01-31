51488175bc9d6b64a3bfa41de501997a91e97954
Josh Gillette <joshtgillette@gmail.com>
2025-01-30 21:32:23 -0500

    Add output size param to neural net

--- neural_network.py
+++ neural_network.py
@@ -5,13 +5,13 @@ class NeuralNetwork(nn.Module):
 
     EMBEDDING_SIZE = 50
 
-    def __init__(self, vocab_size, encoding_size):
+    def __init__(self, vocab_size, encoding_size, output_size):
         super().__init__()
 
         self.embedding = nn.Embedding(vocab_size, self.EMBEDDING_SIZE)
         self.flatten = nn.Flatten()
         self.stack = nn.Sequential(
-            nn.Linear(encoding_size * self.EMBEDDING_SIZE, 3),
+            nn.Linear(encoding_size * self.EMBEDDING_SIZE, output_size),
         )
 
 
